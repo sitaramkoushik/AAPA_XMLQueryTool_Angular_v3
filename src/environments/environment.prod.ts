959d7da499502517dc901ef4017adb0f285b69eb
dgoganty <dharani.g@rknowsys.com>
2021-06-11 20:10:14 +0530

    #40484 XML Query works on Prod and fails on Staging

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   login : "https://ms.myplace4parts.com/prod/xmlQueryTool/userLogin",
-  baseurl: "https://ms.myplace4parts.com/prod/xmlQueryTool"
-
-
+  baseurl: "https://ms.myplace4parts.com/prod/xmlQueryTool",
+  stagingurl: "https://ms.myplace4parts.com/op/xmlQueryTool"
 };
